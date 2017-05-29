7a2356f6482b3b65209bf6986b286df6f4c5d329
Mark Sanders <mark.something@gmail.com>
2017-05-29 03:00:22 +0200

    A little code cleanup and checking if we need to do anything before looping through parts of data.raw.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -15,47 +15,50 @@ local biter_corspes = { -- 60 * 60 * 15
 	"-spiter-", -- Blame McGuten
 	"-worm-",
 }
-for k,v in pairs(data.raw.corpse) do
-	log("Corpse: " .. v.name)
-end
 
-for k,v in pairs(data.raw.corpse) do
-	if settings.startup["Noxys_Fading-tree-stump-fading-time"].value > 0 then
-		if k:find("-stump", -6) then
-			if data.raw.corpse[k].time_before_removed then
-				data.raw.corpse[k].time_before_removed = settings.startup["Noxys_Fading-tree-stump-fading-time"].value
+if settings.startup["Noxys_Fading-tree-stump-fading-time"].value + 
+	settings.startup["Noxys_Fading-biter-fading-time"].value +
+	settings.startup["Noxys_Fading-building-fading-time"].value +
+	settings.startup["Noxys_Fading-rail-fading-time"].value +
+	settings.startup["Noxys_Fading-character-fading-time"].value > 0 then
+	for k,v in pairs(data.raw.corpse) do
+		if settings.startup["Noxys_Fading-tree-stump-fading-time"].value > 0 then
+			if k:find("-stump", -6) then
+				if data.raw.corpse[k].time_before_removed then
+					data.raw.corpse[k].time_before_removed = settings.startup["Noxys_Fading-tree-stump-fading-time"].value
+				end
 			end
 		end
-	end
-	if settings.startup["Noxys_Fading-biter-fading-time"].value > 0 then
-		for _,s in pairs(biter_corspes) do
-			if k:find(s) then
+		if settings.startup["Noxys_Fading-biter-fading-time"].value > 0 then
+			for _,s in pairs(biter_corspes) do
+				if k:find(s) then
+					if data.raw.corpse[k].time_before_removed then
+						data.raw.corpse[k].time_before_removed = settings.startup["Noxys_Fading-biter-fading-time"].value
+					end
+				end
+			end
+		end
+		if settings.startup["Noxys_Fading-building-fading-time"].value > 0 then
+			if building_remnants[k] then
 				if data.raw.corpse[k].time_before_removed then
-					data.raw.corpse[k].time_before_removed = settings.startup["Noxys_Fading-biter-fading-time"].value
+					data.raw.corpse[k].time_before_removed = settings.startup["Noxys_Fading-building-fading-time"].value
 				end
 			end
 		end
 	end
-	if settings.startup["Noxys_Fading-building-fading-time"].value > 0 then
-		if building_remnants[k] then
-			if data.raw.corpse[k].time_before_removed then
-				data.raw.corpse[k].time_before_removed = settings.startup["Noxys_Fading-building-fading-time"].value
+	if settings.startup["Noxys_Fading-rail-fading-time"].value > 0 then
+		for k,v in pairs(data.raw["rail-remnants"]) do
+			if data.raw["rail-remnants"][k].time_before_removed then
+				data.raw["rail-remnants"][k].time_before_removed = settings.startup["Noxys_Fading-rail-fading-time"].value
+				data.raw["rail-remnants"][k].time_before_shading_off = math.ceil(settings.startup["Noxys_Fading-rail-fading-time"].value / 45)
 			end
 		end
 	end
-end
-if settings.startup["Noxys_Fading-rail-fading-time"].value > 0 then
-	for k,v in pairs(data.raw["rail-remnants"]) do
-		if data.raw["rail-remnants"][k].time_before_removed then
-			data.raw["rail-remnants"][k].time_before_removed = settings.startup["Noxys_Fading-rail-fading-time"].value
-			data.raw["rail-remnants"][k].time_before_shading_off = math.ceil(settings.startup["Noxys_Fading-rail-fading-time"].value / 45)
-		end
-	end
-end
-if settings.startup["Noxys_Fading-character-fading-time"].value > 0 then
-	for k,v in pairs(data.raw["character-corpse"]) do
-		if data.raw["character-corpse"][k].time_to_live then
-			data.raw["character-corpse"][k].time_to_live = settings.startup["Noxys_Fading-character-fading-time"].value
+	if settings.startup["Noxys_Fading-character-fading-time"].value > 0 then
+		for k,v in pairs(data.raw["character-corpse"]) do
+			if data.raw["character-corpse"][k].time_to_live then
+				data.raw["character-corpse"][k].time_to_live = settings.startup["Noxys_Fading-character-fading-time"].value
+			end
 		end
 	end
 end
